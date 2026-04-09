bb6e4edecf906a7e11b2c123cb37d2cad8b77f1d
hshkera <hshkera@gmail.com>
2026-04-09 12:38:09 +0400

    Add TestNG annotations and improve test structure

---
+++ src/test/java/com/shkera/tests/AnnotationsTest.java
@@ -0,0 +1,33 @@
+package test.java.com.shkera.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AnnotationsTest {
+
+    @BeforeMethod
+    public void setUp() {
+        System.out.println("Setting up before test");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("Cleaning up after test");
+    }
+
+    @Test
+    public void numberComparisonShouldPass() {
+        System.out.println("Running number comparison test");
+        Assert.assertTrue(20 > 10, "Expected 20 to be greater than 10");
+    }
+
+    @Test
+    public void textShouldMatch() {
+        System.out.println("Running text match test");
+        String actual = "TestNG";
+        String expected = "TestNG";
+        Assert.assertEquals(actual, expected, "Texts do not match");
+    }
+}
--- src/test/java/com/shkera/tests/BasicTest.java
+++ src/test/java/com/shkera/tests/BasicTest.java
@@ -6,26 +6,26 @@ import org.testng.annotations.Test;
 public class BasicTest {
 
     @Test
-    public void additionTest() {
+    public void additionShouldReturnCorrectResult() {
         int result = 2 + 3;
         Assert.assertEquals(result, 5, "Addition failed");
     }
 
     @Test
-    public void stringTest() {
+    public void stringsShouldMatchCorrectly() {
         String actual = "automation";
         String expected = "automation";
         Assert.assertEquals(actual, expected, "Strings do not match");
     }
 
     @Test
-    public void booleanTest() {
+    public void booleanConditionShouldEvaluateToTrue() {
         boolean condition = (10 > 5);
         Assert.assertTrue(condition, "Condition should be true");
     }
 
     @Test
-    public void arrayLengthTest() {
+    public void arrayShouldHaveExpectedLength() {
         int[] numbers = {1, 2, 3, 4};
         Assert.assertEquals(numbers.length, 4, "Array length mismatch");
     }
